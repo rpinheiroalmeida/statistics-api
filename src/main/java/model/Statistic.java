fcdce24bf66090e06fb8831b7a99d5966bc2de82
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2018-07-08 13:40:33 -0300

    Changing the class Statistic to be final

--- src/main/java/model/Statistic.java
+++ src/main/java/model/Statistic.java
@@ -3,49 +3,43 @@ package model;
 import java.math.BigDecimal;
 import java.time.Instant;
 
-public class Statistic {
+public final class Statistic {
 
     public static final long SIXTY_SENCONDS_IN_MILLISECONDS = 60000L;
 
     private final int count;
     private final Instant timestamp;
-
-    private double max = Double.MIN_VALUE;
-    private double min = Double.MAX_VALUE;
-    private BigDecimal totalAmount = BigDecimal.ZERO.setScale(2, BigDecimal.ROUND_UP);
+    private final double max;
+    private final double min;
+    private final BigDecimal totalAmount;
 
 
     public Statistic(double amount, int count, Instant timestamp) {
         this.count = count;
         this.timestamp = timestamp.plusMillis(SIXTY_SENCONDS_IN_MILLISECONDS);
 
-        this.totalAmount = this.totalAmount.add(BigDecimal.valueOf(amount));
-        this.max = Math.max(max, amount);
-        this.min = Math.min(min, amount);
+        this.totalAmount = BigDecimal.valueOf(amount);
+        this.max = Math.max(Double.MIN_VALUE, amount);
+        this.min = Math.min(Double.MAX_VALUE, amount);
+    }
+
+    private Statistic(BigDecimal totalAmount, int count, Instant timestamp, double min, double max, double amount) {
+        this.count = count;
+        this.timestamp = timestamp.plusMillis(SIXTY_SENCONDS_IN_MILLISECONDS);
+        this.totalAmount = totalAmount;
+        this.min = Math.min(amount, min);
+        this.max = Math.max(amount, max);
     }
 
     public Statistic sum(double amount) {
-        return new Statistic(amount,
-                    this.count +1,
-                    timestamp.minusMillis(SIXTY_SENCONDS_IN_MILLISECONDS))
-                .add(this.totalAmount.add(BigDecimal.valueOf(amount)))
-                .max(this.max, amount)
-                .min(this.min, amount);
-    }
-
-    private Statistic min(double oldAlmount, double newAmount) {
-        this.min = Math.min(oldAlmount, newAmount);
-        return this;
-    }
-
-    private Statistic max(double oldAmount, double newAmount) {
-        this.max = Math.max(oldAmount, newAmount);
-        return this;
-    }
-
-    private Statistic add(BigDecimal amount) {
-        this.totalAmount = amount;
-        return this;
+        return new Statistic(
+                this.totalAmount.add(BigDecimal.valueOf(amount)),
+                count + 1,
+                timestamp.minusMillis(SIXTY_SENCONDS_IN_MILLISECONDS),
+                this.min,
+                this.max,
+                amount
+        );
     }
 
     public boolean shouldBelongToNextSixtySeconds(long nextTimestamp) {
