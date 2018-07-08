45755d9675fefd0d3292a6c6b8c6d12e0581bb5c
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2018-07-08 20:10:49 -0300

    Removing method does not used

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,10 +9,6 @@ public class Main {
 
     private static TransactionService service = new TransactionService();
 
-    public static void stop() {
-        Spark.stop();
-    }
-
     public static void main(String[] args) {
         start();
     }
